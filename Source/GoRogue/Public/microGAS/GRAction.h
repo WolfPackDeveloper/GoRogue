6bc0f050224906d23a36dc3a659ac9da317bf0cc
Khrustalev V <khrustalev.v@wolfpackgames.ru>
2021-11-26 15:30:58 +0300

    WOD commit

--- Source/GoRogue/Public/microGAS/GRAction.h
+++ Source/GoRogue/Public/microGAS/GRAction.h
@@ -35,6 +35,9 @@ class GOROGUE_API UGRAction : public UObject
 
 protected:
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI")
+	UTexture2D* Icon;
+
 	/* Tags added to owning actor when activated, and removed, when action stops. */
 	UPROPERTY(EditDefaultsOnly, Category = "Tags")
 	FGameplayTagContainer GrantsTags;
